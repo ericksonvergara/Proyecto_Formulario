9bea9c394e6ecbb869241f0e5b693b5ed06e4c74
ericksonvergara <desarrollador8@redsuelva.com>
2024-02-16 18:02:38 -0500

    inicio

---
+++ src/main/java/formulario/formulario.java
@@ -0,0 +1,17 @@
+package src.main.java.formulario;
+
+import java.util.Scanner;
+
+public class formulario {
+
+    public static void registro(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+
+    }
+
+
+
+}
