7e433dfe1c68e508835a6887168d813424fe5c84
ericksonvergara <desarrollador8@redsuelva.com>
2024-02-17 13:07:50 -0500

    añadiendo mas cambios al formulario

--- src/main/java/formulario/Formulario.java
+++ src/main/java/formulario/Formulario.java
@@ -1,6 +1,10 @@
 package src.main.java.formulario;
 
+import java.util.Date;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.regex.Pattern;
 
 public class Formulario {
 
@@ -20,6 +24,42 @@ public class Formulario {
         imprimirTipoDoc();
         int opcion = sc.nextInt();
 
+        sc.nextLine();
+
+        boolean numDocumentoValido = false;
+        do {
+            System.out.println("Digite el número de documento:");
+            String numDocumento = sc.nextLine();
+            if(!validarCampo(numDocumento, "\\d{1,10}")){
+                System.out.println("El número de documento ingresado es inválido. \nDebe contener solo dígitos y tener máximo 10 caracteres.");
+            }else {
+                numDocumentoValido = true;
+            }
+        } while (!numDocumentoValido);
+
+
+        System.out.println("Ingrese su fecha de nacimiento en formato dd/MM/yyyy:");
+        Date fechaNacimiento = solicitarFecha(sc);
+
+        System.out.println("Digite su genero");
+        String genero = sc.nextLine();
+
+        System.out.println("Ingrese su dirección de domicilio:");
+        String direccion = sc.nextLine();
+
+        System.out.println("Ingrese su correo electronico:");
+        String email = sc.nextLine();
+
+        boolean telefonoValido = false;
+        do {
+            System.out.println("numero de Telefono:");
+            String telefono = sc.nextLine();
+            if (!validarCampo(telefono, "\\d{1,10}")){
+                System.out.println("El número de teléfono ingresado es inválido. \nDebe contener solo dígitos y tener máximo 10 caracteres.");
+            }else {
+                telefonoValido = true;
+            }
+        }while (!telefonoValido);
 
     }
 
@@ -28,5 +68,30 @@ public class Formulario {
         for (int i = 1; i  < TipoDocumento.values().length; i++){
             System.out.println(i + ". "+TipoDocumento.values()[i].getNombreDoc());
         }
+    }
+
+    public static Date solicitarFecha(Scanner sc){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy");
+        Date fecha = null;
+
+        boolean fechaValida = false;
+        while (!fechaValida){
+            String fechaString = sc.nextLine();
+            try {
+                fecha = dateFormat.parse(fechaString);
+                fechaValida = true;
+            } catch (ParseException e){
+                System.out.println("Error: Formato de fecha invalido!. Ingrese nuevamente");
+            }
+        }
+        return fecha;
+    }
+
+//    public static boolean validarNumDocumento(String numDocumento){
+//        return Pattern.matches("\\d{1,10}", numDocumento);
+//    }
+
+    public static boolean validarCampo(String campo, String patron) {
+        return Pattern.matches(patron, campo);
     }
 }
